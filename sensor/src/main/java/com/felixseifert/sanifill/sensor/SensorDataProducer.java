2957fa2915fbd59f148a134140d3265e7f23c8d5
Felix Seifert <mail@felix-seifert.com>
2021-05-04 12:00:59 +0200

    Create Sensor Imitation Which Publishes Data (#1)
    * Application imitates `Sensor` which measures filling in percentage of liquid container
    * Regularly publishes messages with object `SensorData` with decreasing filling to Kafka channel `sensors`
    * Call of REST API refills measured container
    * Allow custom sensor ID via config property (optional)
    * Add logging information in resource/data producer about published messages and refills
    * Describe message channel, custom ID and port in README

---
+++ sensor/src/main/java/com/felixseifert/sanifill/sensor/SensorDataProducer.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2021 Felix Seifert <mail@felix-seifert.com> (https://felix-seifert.com)
+ *
+ * This programme is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any
+ * later version.
+ *
+ * This programme is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.felixseifert.sanifill.sensor;
+
+import com.felixseifert.sanifill.sensor.sensor.Sensor;
+import com.felixseifert.sanifill.sensor.sensor.SensorData;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.reactive.messaging.annotations.Broadcast;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.jboss.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.time.Duration;
+
+@ApplicationScoped
+public class SensorDataProducer {
+
+    private static final Logger LOGGER = Logger.getLogger(SensorDataProducer.class);
+
+    @Inject
+    Sensor sensor;
+
+    @Outgoing("sensors")
+    @Broadcast
+    public Multi<SensorData> produceSensorData() {
+        return Multi.createFrom()
+                .ticks().every(Duration.ofSeconds(20))
+                .onOverflow().drop()
+                .map(this::publishSensorData);
+    }
+
+    private SensorData publishSensorData(Long tick) {
+        SensorData sensorData = sensor.getCurrentData();
+        LOGGER.infov("Filling of sensor {0} at {1}: {2}",
+                sensorData.getSensorId(),
+                sensorData.getDateTime().toString(),
+                sensorData.getData());
+        return sensorData;
+    }
+}
---
+++ sensor/src/main/java/com/felixseifert/sanifill/sensor/sensor/SensorImpl.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2021 Felix Seifert <mail@felix-seifert.com> (https://felix-seifert.com)
+ *
+ * This programme is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any
+ * later version.
+ *
+ * This programme is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.felixseifert.sanifill.sensor.sensor;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Singleton;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Singleton
+public class SensorImpl implements Sensor {
+
+    private static final Logger LOGGER = Logger.getLogger(SensorImpl.class);
+
+    private double currentFilling;
+
+    private String sensorId;
+
+    @ConfigProperty(name = "sensor.id")
+    Optional<String> cmdSensorId;
+
+    @PostConstruct
+    public void postConstruct() {
+        currentFilling = ThreadLocalRandom.current().nextDouble(0.0, 1.0);
+        sensorId = createSensorId();
+        LOGGER.infov("Sensor created with ID {0} and filling {1}.", sensorId, currentFilling);
+    }
+
+    private String createSensorId() {
+        return cmdSensorId.orElse(UUID.randomUUID().toString());
+    }
+
+    @Override
+    public String getSensorId() {
+        return sensorId;
+    }
+
+    @Override
+    public SensorData getCurrentData() {
+        return new SensorData(sensorId, LocalDateTime.now(), getCurrentFilling());
+    }
+
+    private double getCurrentFilling() {
+        if(currentFilling > 0) {
+            reduceCurrentFilling();
+        }
+        return Math.max(0.0, currentFilling);
+    }
+
+    private void reduceCurrentFilling() {
+        currentFilling -= (1.0 / (ThreadLocalRandom.current().nextInt(100, 160)));
+    }
+
+    @Override
+    public SensorData resetData() {
+        currentFilling = 1;
+        return getCurrentData();
+    }
+}
