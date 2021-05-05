8441aab84d363550ab1505b44d9d28521f9f0583
Felix Seifert <mail@felix-seifert.com>
2021-05-05 15:12:09 +0200

    Read Messages From Kafka Topic `sensors`
    * Add dependecy for `spring-kafka` to consume messages from Kafka channel
    * Listen to new messages on channel `sensors`
    * Send received messages to `SensorService`  scaffold

---
+++ frontend/src/main/java/com/felixseifert/sanifill/frontend/kafka/KafkaSensorsListener.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2021 Felix Seifert <mail@felix-seifert.com> (https://felix-seifert.com)
+ *
+ * This programme is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any
+ * later version.
+ *
+ * This programme is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.felixseifert.sanifill.frontend.kafka;
+
+import com.felixseifert.sanifill.frontend.model.SensorData;
+import com.felixseifert.sanifill.frontend.service.SensorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaSensorsListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaSensorsListener.class);
+
+    @Autowired
+    private SensorService sensorService;
+
+    @KafkaListener(topics = "sensors", groupId = "sanifill-sensors",
+            containerFactory = "kafkaListenerContainerFactory")
+    public void listenToSensorsTopic(SensorData sensorData) {
+        LOGGER.info("Received sensorData: {}", sensorData);
+        sensorService.sendSensorDataToGui(sensorData);
+    }
+}
