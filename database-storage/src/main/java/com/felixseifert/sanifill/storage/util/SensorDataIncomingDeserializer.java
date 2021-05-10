69bd361ab165fd86be2e8332c6fb2a684d039141
Felix Seifert <mail@felix-seifert.com>
2021-05-10 07:14:32 +0200

    Move Deserializer to Package `util`

--- database-storage/src/main/java/com/felixseifert/sanifill/storage/util/SensorDataIncomingDeserializer.java
+++ database-storage/src/main/java/com/felixseifert/sanifill/storage/util/SensorDataIncomingDeserializer.java
@@ -15,8 +15,9 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package com.felixseifert.sanifill.storage.model;
+package com.felixseifert.sanifill.storage.util;
 
+import com.felixseifert.sanifill.storage.model.SensorDataIncoming;
 import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer;
 
 public class SensorDataIncomingDeserializer extends ObjectMapperDeserializer<SensorDataIncoming> {
